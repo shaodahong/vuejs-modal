3975e6adc1a0172a7e3d424d5f01662b6875376c
shaodahong <337097324@qq.com>
2017-07-24 14:47:13 +0800

    have a base idea

--- index.js
+++ index.js
@@ -1,12 +1,66 @@
+import confirm from '../../components/modals/confirm.vue';
+
 var modal = {
     install: function (Vue, options) {
 
-        var defaultOptions = Object.assgin({
-            name: '$modal'
-        },options)
-        Vue.prototype[defaultOptions.name] = function () {
+        var defaultOptions = Object.assign({
+            name: '$modal',
+            id: 'modal'
+        }, options)
+
+        this.init(defaultOptions.id);
+
+        console.log('defaultOptions', defaultOptions, Vue.prototype);
+
+        var modals = Vue.prototype[defaultOptions.name] = {};
+
+        console.log('confirm', confirm)
+        confirm.methods.$ok = function () {
+            console.log('osdjcopsjpojcpoj')
+        }
+        modals.confirm = options => {
+            return new Promise((res, rej) => {
+                console.log('options', options)
+                new Vue({
+                    render: h => h(confirm, {
+                        props: options,
+                        on: {
+                            abc: function (v) {
+                                console.log('abc')
+                                res(v)
+                            }
+                        }
+                    })
+                }).$mount('#' + defaultOptions.id)
+
+                this.init(defaultOptions.id)
+            })
+
 
         }
+    
+
+
+
+
+
+
+
+        // Object.keys(defaultOptions.modals).forEach(function (v) {
+        //     modals[v] = function (box) {
+        //         Vue.component(v, defaultOptions.modals[v])
+        //         console.log('box', box.content)
+        //         console.log(v, defaultOptions.modals[v])
+
+        //         // document.getElementsByTagName('body')[0].appendChild('<confirm></confirm>')
+        //     }
+
+        // })
+    },
+    init: name => {
+        const div = document.createElement('div');
+        div.setAttribute('id', name || 'modal');
+        document.getElementsByTagName('body')[0].appendChild(div)
     }
 }
 
